a10ec743ca254891f7c315412a611320be34f5cc
Raufir Ahmed Shanto <shanto@Raufirs-MacBook-Pro.local>
2020-07-18 13:56:58 -0230

    Upto exercise 1.12

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,9 @@
 # bounce.py
-#
-# Exercise 1.5
+height = 100
+bounce = 1
+
+while bounce <=10:
+    height = (3/5) * height
+    print(bounce , round(height,4))
+    bounce = bounce + 1
+
---
+++ Work/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+while principal > 0:
