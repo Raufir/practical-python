594d7acc6a30602429b291d658ecfa10e895e755
Raufir Ahmed Shanto <shanto@Raufirs-MacBook-Pro.local>
2020-07-19 16:48:05 -0230

    Upto exercise 1.28

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,17 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0.0
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f)
+    for line in f:
+        row = line.split(',')
+        print(row)
+        nshares = int(row[1])
+        price = float(row[2])
+        total_cost= total_cost + nshares * price
+
+    print('Total cost', total_cost)
+       
