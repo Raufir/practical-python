5b82c3255e81fe1ddae2759e710a0e186dac472b
Raufir Ahmed Shanto <shanto@Raufirs-MacBook-Pro.local>
2020-07-21 03:42:06 -0230

    Upto exercise 2.5

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,25 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+import sys
+def read_portfolio(filename):
+
+    portfolio = []
+    with open(filename) as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+
+        for row in rows:
+            stock = {
+                 'name'   : row[0],
+                 'shares' : int(row[1]),
+                 'price'   : float(row[2])
+            }
+            portfolio.append(stock)
+
+    return portfolio
+           
+    
+    return portfolio
