6eebcc9ce0a78d70815864c45a6862b7349f166c
Raufir Ahmed Shanto <shanto@Raufirs-MacBook-Pro.local>
2020-07-21 04:31:23 -0230

    Upto exercise 2.6

--- Work/report.py
+++ Work/report.py
@@ -18,8 +18,18 @@ def read_portfolio(filename):
                  'price'   : float(row[2])
             }
             portfolio.append(stock)
-
-    return portfolio
            
     
     return portfolio
+
+def read_prices(filename):
+     prices = {}
+     with open(filename) as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+
+     return prices
