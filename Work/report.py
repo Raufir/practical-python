10a1478dd508076b96b20b2e153fd4c2f0f5fb8b
Raufir Ahmed Shanto <shanto@Raufirs-MacBook-Pro.local>
2020-07-22 02:58:58 -0230

    exercise 2.9

--- Work/report.py
+++ Work/report.py
@@ -38,7 +38,7 @@ portfolio = read_portfolio('Data/portfolio.csv')
 prices    = read_prices('Data/prices.csv')
 
 # Calculate the total cost of the portfolio
-total_cost = 0.0
+'''total_cost = 0.0
 for s in portfolio:
     total_cost += s['shares']*s['price']
 
@@ -47,7 +47,19 @@ print('Total cost', total_cost)
 # Compute the current value of the portfolio
 total_value = 0.0
 for s in portfolio:
-    total_value += s['shares']*prices[s['name']]
+    total_value += s['shares']*prices[s['name']]'''
 
-print('Current value', total_value)
-print('Gain', total_value - total_cost)
+#print('Current value', total_value)
+#print('Gain', total_value - total_cost)
+
+def make_report(portfolio, prices):
+    rows = []
+    for stock in portfolio:
+        current_price = prices[stock['name']]
+        change = current_price - stock['price']
+        summary = (stock['name'], stock['shares'], current_price, change)
+        rows.append(summary)    
+    return rows
+
+
+report = make_report(portfolio, prices)
