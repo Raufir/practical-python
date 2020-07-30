244d6fb962f76b507fa7929c1aa3a347796e4b9a
Raufir Ahmed Shanto <shanto@Raufirs-MacBook-Pro.local>
2020-07-29 23:04:02 -0230

    solved 3.15 and 3.16

--- Work/pcost.py
+++ Work/pcost.py
@@ -13,12 +13,14 @@ def portfolio_cost(filename):
     portfolio = report.read_portfolio(filename)
     return sum([s['shares']*s['price'] for s in portfolio])
 
-if len(sys.argv) == 2:
-    filename = sys.argv[1]
-else:
-    filename = 'Data/portfolio.csv'
+def main(args):
+    if len(args) != 2:
+        raise SystemExit('Usage: %s portfoliofile' % args[0])
+    filename = args[1]
+    print('Total cost:', portfolio_cost(filename))
 
-cost = portfolio_cost(filename)
-print('Total cost', cost)
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
        
 
